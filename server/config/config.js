7ec707f2ec1d392f10be783374a43cb50e4a1d14
manelmendez <maneeel93@gmail.com>
2019-03-27 14:51:18 +0100

    docker fix

--- server/config/config.js
+++ server/config/config.js
@@ -1,5 +1,5 @@
 module.exports = {
   port: process.env.PORT || 9000,
-  db: process.env.MONGODB_URI || 'mongodb://localhost:27017/matchAnalizer',
+  db: process.env.MONGODB_URI || 'mongodb://localhost:27017/matchAnalyzer',
   SECRET_TOKEN: 'claveincreiblementecomplicada'
 }
