22bdc090b3d7c881474966138f2caa93aedaa21a
manelmendez <maneeel93@gmail.com>
2019-07-01 23:01:42 +0200

    some changes

--- client/src/assets/constants/constants.js
+++ client/src/assets/constants/constants.js
@@ -1,7 +1,8 @@
 module.exports = {
-  LOCAL_API_ADDRESS : 'http://127.0.0.1:9000/api/',
-  LOCAL_ADDRESS : 'http://127.0.0.1:9000/',
-  // LOCAL_ADDRESS : 'http://37.133.253.172:9000/api/',
+  // LOCAL_API_ADDRESS : 'http://127.0.0.1:9000/api/',
+  // LOCAL_ADDRESS : 'http://127.0.0.1:9000/',
+  LOCAL_API_ADDRESS : 'http://37.133.124.200:9000/api/',
+  LOCAL_ADDRESS : 'http://37.133.124.200:9000/',
   theme1: {
     PRIMARY_COLOR : '#6200EE',
     PRIMARY_DARK_COLOR : '#3700B3',
