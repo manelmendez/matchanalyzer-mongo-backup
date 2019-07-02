1bae1e6e8b2271e6de79da36cc7a4c16f04b7cfe
manelmendez <maneeel93@gmail.com>
2019-07-02 10:34:53 +0200

    online db connection

--- server/config/config.js
+++ server/config/config.js
@@ -1,5 +1,6 @@
 module.exports = {
   port: process.env.PORT || 9000,
-  db: process.env.MONGODB_URI || 'mongodb://localhost:27017/matchAnalyzer',
+  // db: process.env.MONGODB_URI || 'mongodb://localhost:27017/matchAnalyzer',
+  db: process.env.MONGODB_URI || 'mongodb+srv://manel:manel@matchanalyzer-fusdr.gcp.mongodb.net/matchanalyzer?retryWrites=true&w=majority',
   SECRET_TOKEN: 'claveincreiblementecomplicada'
 }
