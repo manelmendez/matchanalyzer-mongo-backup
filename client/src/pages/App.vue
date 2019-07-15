6b1629499c51e4bda032c6af77f9796b57592504
manelmendez <maneeel93@gmail.com>
2019-07-15 12:30:10 +0200

    App

--- client/src/pages/App.vue
+++ client/src/pages/App.vue
@@ -15,31 +15,27 @@ import Layout from '../layouts/Layout.vue'
 import Login from './login.vue'
 import Snackbar from '../components/flashmessages/Snackbar'
 
-  import { mapGetters } from 'vuex'
-  export default {
-    name: "App",
-    components: {
-      Layout,
-      Login,
-      Snackbar
-    },
-    data: () => ({
-      drawer: true,
-    }),
-    computed: {
-      ...mapGetters([
-        'isLogged',
-        'snackbar'
-      ]),
+import { mapGetters } from 'vuex'
+export default {
+  name: "App",
+  components: {
+    Layout,
+    Login,
+    Snackbar
+  },
+  data: () => ({
+    drawer: true,
+  }),
+  computed: {
+    ...mapGetters([
+      'isLogged',
+      'snackbar'
+    ]),
 
-    }
   }
+}
 
 </script>
 
 <style>
-  .app{
-    min-height: 100%;
-    height: 100%;
-  }
 </style>
